41e5f4a0015c41bf145d4674a72eff949b46576b
keyready <93424230+keyready@users.noreply.github.com>
2023-04-19 13:00:36 +0300

    create-read products

--- src/pages/mainPage/ui/MainPage.tsx
+++ src/pages/mainPage/ui/MainPage.tsx
@@ -1,9 +1,26 @@
 import { Page } from 'widgets/Page/Page';
 import { InputGroup, Form, Button } from 'react-bootstrap';
-import { FormEvent, useEffect } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
+import { Card } from 'shared/UI/Card/Card';
+import { HStack } from 'shared/UI/Stack';
+
+interface IProduct {
+    name?: string;
+    price?: number;
+    description?: string;
+    image?: string
+}
 
 const MainPage = () => {
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:1337/products')
+            .then((res) => res.json())
+            .then((res) => setProducts(res));
+    }, [products]);
+
+    const handleSubmitUser = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
 
@@ -12,12 +29,22 @@ const MainPage = () => {
             body: formData,
         });
     };
+    const handleSubmitProduct = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+
+        fetch('http://localhost:1337/create_product', {
+            method: 'POST',
+            body: formData,
+        });
+    };
 
     return (
         <Page>
+            <h1>добавить пользователя</h1>
             <Form
                 encType="multipart/form-data"
-                onSubmit={handleSubmit}
+                onSubmit={handleSubmitUser}
             >
                 <InputGroup className="mb-3">
                     <InputGroup.Text id="basic-addon1">Имя</InputGroup.Text>
@@ -50,13 +77,74 @@ const MainPage = () => {
                 <Form.Group>
                     <Form.Label>Загрузите аватар</Form.Label>
                     <Form.Control
-                        name="avatar"
-                        id="avatar"
+                        name="image"
+                        id="image"
                         type="file"
                     />
                 </Form.Group>
                 <Button type="submit">Отправить</Button>
             </Form>
+            <hr />
+            <br />
+            <br />
+            <br />
+
+            <h1>добавить продукт</h1>
+            <Form
+                encType="multipart/form-data"
+                onSubmit={handleSubmitProduct}
+            >
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">Название</InputGroup.Text>
+                    <Form.Control
+                        name="name"
+                        placeholder="Название"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">Описание</InputGroup.Text>
+                    <Form.Control
+                        name="description"
+                        placeholder="Описание"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">Стоимость</InputGroup.Text>
+                    <Form.Control
+                        type="number"
+                        name="price"
+                        placeholder="Стоимость"
+                    />
+                </InputGroup>
+                <Form.Group>
+                    <Form.Label>Загрузите фото</Form.Label>
+                    <Form.Control
+                        name="image"
+                        id="image"
+                        type="file"
+                    />
+                </Form.Group>
+                <Button type="submit">Отправить</Button>
+            </Form>
+            <hr />
+            <br />
+            <br />
+            <br />
+
+            <h1>Список всех продуктов</h1>
+            {products.map((product) => (
+                <Card>
+                    <HStack max>
+                        <h2>{product.name}</h2>
+                        <img src={product.image} alt="" />
+                    </HStack>
+                    <p>{product.description}</p>
+                    <p>
+                        Стоимость:
+                        {product.price}
+                    </p>
+                </Card>
+            ))}
         </Page>
     );
 };
